38194df32bd0c9211c5a7db676a14934d81a36fd
JurguteJak <jurgutejak@gmail.com>
2024-07-09 09:29:57 +0300

    chat

---
+++ chat/chat.js
@@ -0,0 +1,37 @@
+const data = [
+    {
+        person: 'other',
+        message: 'Labas!'
+    }, {
+        person: 'me',
+        message: 'Labas ir tau!'
+    },
+    {
+        person: 'other',
+        message: 'Ka tu?'
+    },
+    {
+        person: 'me',
+        message: 'Nieko, o tu?'
+    },
+    {
+        person: 'other',
+        message: 'Irgi nieko...'
+    },
+];
+
+const chatDOM = document.querySelector('.chat');
+
+let HTML = '';
+
+for (const msg of data) {
+    const text = msg.message
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+        .replaceAll('"', '&quot;')
+        .replaceAll("'", '&apos;')
+        .replaceAll('=', '?')
+    HTML += `<div class="msg ${msg.person}">${text}</div>`;
+}
+
+chatDOM.innerHTML = HTML;
